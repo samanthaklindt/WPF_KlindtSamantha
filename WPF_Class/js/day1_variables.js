7c9e5f9e445eb3b80a852508294d25f09d4504aa
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-10-28 11:51:43 -0400

    NumericTypes
    Class 1: Numbers & Number Casings

--- WPF_Class/js/day1_variables.js
+++ WPF_Class/js/day1_variables.js
@@ -8,6 +8,24 @@
 
 
 var name;
-name ='Samantha Klindt';
+name ='Samantha Klindt'; //string example
+var age = 18; //number example
+var pet = 'Sawyer';
+var petAge = 7;
 
-console.log(name);
+// *NOTES* the below(age) variables convert the above(age) variables to add one... manipulating the number rather than resetting it.
+
+console.log(age);
+age = age +1;
+console.log(age);
+age = age -10;
+console.log(age);
+age = age *2;
+console.log(age);
+
+
+console.log(parseInt(petAge + Number(age) + age*1);
+
+// *NOTES* multiply a number by a string it will result in a number
+
+
