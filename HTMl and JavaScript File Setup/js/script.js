af71bae2ca6163a6663748ae901e978341c765ba
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-16 12:08:25 -0500

    Expressions Assignment
    Commit #3 - File organization

--- HTMl and JavaScript File Setup/js/script.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by samanthaklindt on 10/30/13.
- */
-
-//JavaScript
-
-//this changes the css of your body tag to be green
-console.querySelector("body").style.backgroundColor= "#00FF00";
