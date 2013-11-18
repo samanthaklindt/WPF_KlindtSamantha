f56502f5570cd51723f49a700fe54db7c02c0c71
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 16:23:19 -0500

    Commit #1
    Conditionals Assignment

---
+++ Conditionals_Assignment/wacky.js
@@ -0,0 +1,13 @@
+/**
+ * Created by samanthaklindt on 11/18/13.
+ */
+
+//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded
+//values
+
+//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block
+// 4. Else block 5. Conditional operator other than equals
+
+//All prompted values are checked to make sure they look okay.
+
+//Each line of code has a meaningful comment.
