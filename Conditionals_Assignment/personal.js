979626aa03cbb468cef7b549f61cac2a4ee1976b
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 18:20:44 -0500

    Commit #14
    Conditionals Assignment

--- Conditionals_Assignment/personal.js
+++ Conditionals_Assignment/personal.js
@@ -18,10 +18,10 @@ var year= prompt('What year were you born in?'); //This variable prompts the use
 var todaysYear= prompt('What year is it now?'); //This variable prompts the user to input the year they are currently in.
 var age= todaysYear - year; //This variable is subtracting todaysYear from year to get the users age.
 
-console.log('Hello '+name+ '! You are '+age+' years old.');
+console.log('Hello '+name+ '! You are '+age+' years old.'); //This line puts together both the name and age variable to give the user an answer.
 
-if(age<18){
-    console.log('You are a minor!');
+if(age<18){ //This if statement requires the age to be less than 18 in order to print the answer.
+    console.log('You are a minor!'); //This line tells the user they are a minor based on their age which is less than 18.
 } else{(age>18)
     console.log('You are an adult!');
 }
