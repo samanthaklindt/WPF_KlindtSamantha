a3121782df40c3d171fa27573025571645bc26d8
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 18:12:39 -0500

    Commit #12
    Conditionals Assignment

--- Conditionals_Assignment/personal.js
+++ Conditionals_Assignment/personal.js
@@ -26,11 +26,3 @@ if(age<18){
     console.log('You are an adult!');
 }
 
-if(year<2013){
-    console.log('Congratulations, You have some how figured out time travel and are in a past time!');
-}else if(year>=2013){
-    console.log('Congratulations, You have some how figured out time travel and are in a future time!');
-}if(year=2013){
-    console.log('You are in the year 2013, you have not figured out time travel and most likely won\'t until you work even harder!');
-}
-
--- Conditionals_Assignment/wacky.js
+++ Conditionals_Assignment/wacky.js
@@ -11,3 +11,12 @@
 //All prompted values are checked to make sure they look okay.
 
 //Each line of code has a meaningful comment.
+
+
+if(year<2013){
+    console.log('Congratulations, You have some how figured out time travel and are in a past time!');
+}else if(year>=2013){
+    console.log('Congratulations, You have some how figured out time travel and are in a future time!');
+}if(year=2013){
+    console.log('You are in the year 2013, you have not figured out time travel and most likely won\'t until you work even harder!');
+}
