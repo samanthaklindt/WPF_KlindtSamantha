cb67afcdffb9ef5556e43ce429269719f8f68a4f
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 18:18:32 -0500

    Commit #13
    Conditionals Assignment

--- Conditionals_Assignment/wacky.js
+++ Conditionals_Assignment/wacky.js
@@ -12,10 +12,12 @@
 
 //Each line of code has a meaningful comment.
 
+var year= prompt('What year is it?'); //This variable prompts the user to input their current year.
+
 
 if(year<2013){
     console.log('Congratulations, You have some how figured out time travel and are in a past time!');
-}else if(year>=2013){
+}else if(year>2013){
     console.log('Congratulations, You have some how figured out time travel and are in a future time!');
 }if(year=2013){
     console.log('You are in the year 2013, you have not figured out time travel and most likely won\'t until you work even harder!');
