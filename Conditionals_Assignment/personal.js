95bc04fbd6e001ead5bb8d2894f3799838f671f4
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 18:23:01 -0500

    Commit #16
    Conditionals Assignment

--- Conditionals_Assignment/personal.js
+++ Conditionals_Assignment/personal.js
@@ -2,15 +2,7 @@
  * Created by samanthaklindt on 11/18/13.
  */
 
-//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded
-//values
-
-//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block
-// 4. Else block 5. Conditional operator other than equals
-
-//All prompted values are checked to make sure they look okay.
-
-//Each line of code has a meaningful comment.
+//Personal
 
 var name; //This variable stores the prompted variable for name.
 name = prompt('What is your name?'); //This variable prompts the user to input their name.
