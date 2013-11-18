77b745ff1956b1250628d0904b7241c08c7e6e38
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 18:36:18 -0500

    Commit #19
    Conditionals Assignment

--- Conditionals_Assignment/industry.js
+++ Conditionals_Assignment/industry.js
@@ -11,3 +11,14 @@
 //All prompted values are checked to make sure they look okay.
 
 //Each line of code has a meaningful comment.
+
+
+var computer= Math.random()*4;
+var mobileDevice= Math.random()*4;
+var totalDevices= computer + mobileDevice;
+
+if(totalDevices>5){
+    console.log('You have '+totalDevices+' and that is greater than 5... If you get anymore computers or mobile devices you might go crazy!')
+}else{(totalDevices<5)
+    console.log('You have '+totalDevices+' and that is less than 5... You may want to look into getting more computers or mobile devices!')
+}
