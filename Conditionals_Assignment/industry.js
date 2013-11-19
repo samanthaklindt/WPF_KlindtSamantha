1d2a1feaef3af32f3d0794256dc9d1952b6ed9e6
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-18 19:04:59 -0500

    Commit #21
    Conditionals Assignment

--- Conditionals_Assignment/industry.js
+++ Conditionals_Assignment/industry.js
@@ -2,16 +2,7 @@
  * Created by samanthaklindt on 11/18/13.
  */
 
-//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded
-//values
-
-//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block
-// 4. Else block 5. Conditional operator other than equals
-
-//All prompted values are checked to make sure they look okay.
-
-//Each line of code has a meaningful comment.
-
+//Industry
 
 var computer= Math.random()*4; //This variable creates a random output and is multiplied by 4.
 var mobileDevice= Math.random()*10; //This variable creates a random output and is multiplied by 10.
