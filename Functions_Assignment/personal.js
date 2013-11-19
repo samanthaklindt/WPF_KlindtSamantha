d81b4be826a4c483a2727b9b1e5a030f07a83e83
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-19 09:25:25 -0500

    Commit #2
    Functions Assignment

--- Functions_Assignment/personal.js
+++ Functions_Assignment/personal.js
@@ -3,3 +3,14 @@
  */
 
 //Personal
+
+//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values.
+
+//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block 4. Else block 5. Conditional operator other than equals
+
+//Each variant has: 1. At least one working function, 2. ... that contains reusable code 3. ... and is called and used correctly 4. ... and shows a grasp of encapsulation.
+
+//At least one function: 1. Accepts at least 3 parameters 2. Is called with the correct arguments
+
+//1. At least one function returns a value. 2. Return values are meaningful. 3. Return values are assigned or used in a meaningful manner.
+
