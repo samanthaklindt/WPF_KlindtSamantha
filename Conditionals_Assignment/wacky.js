0a98b39dae932d6bfbfecff1d77c56c44c7d9ac5
josephdalton <jdalton@fullsail.com>
2013-11-19 09:17:31 -0500

    JMD Comments

--- Conditionals_Assignment/wacky.js
+++ Conditionals_Assignment/wacky.js
@@ -11,6 +11,6 @@ if(year<2013){
     console.log('Congratulations, You have some how figured out time travel and are in a past time!');
 }else if(year>2013){
     console.log('Congratulations, You have some how figured out time travel and are in a future time!');
-}if(year=2013){
+}if(year=2013){//the useage of an if here is not tied to the previous if/else. It is correct but if you want a condition which is a catch all you need to use an else.
     console.log('You are in the year 2013, you have not figured out time travel and most likely won\'t until you work even harder!');
 }
