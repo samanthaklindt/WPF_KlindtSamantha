7dc3d17f67e0292e0171d01209ddd93308176151
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-21 15:39:40 -0500

    Commit #15
    Functions Assignment

--- Functions_Assignment/personal.js
+++ Functions_Assignment/personal.js
@@ -11,12 +11,15 @@ color= prompt('What is your favorite color?');
 
 function colorGen(color){
 
+    var color;
+    color= prompt('What is your favorite color?');
+
     if(color = 'red'){
-        console.log('Your favorite color is red, like an apple!');
-        if(color = 'blue')
-            console.log('Your favorite color is blue, like the sky!');
-    }else if(color = green){
-        console.log('Your favorite color is green, like the grass!');
+        console.log('Your favorite color is red, like an apple!');}
+        if(color = 'blue'){
+            console.log('Your favorite color is blue, like the sky!');}
+                else if(color = green){
+                     console.log('Your favorite color is green, like the grass!');
     }
     return color;
 }
