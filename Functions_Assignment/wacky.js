4a603866ba4b7488c5051df11caf83f1c436fcba
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-21 13:20:23 -0500

    Commit #7
    Functions Assignment

--- Functions_Assignment/wacky.js
+++ Functions_Assignment/wacky.js
@@ -4,15 +4,6 @@
 
 //Wacky
 
-//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values.
-
-//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block 4. Else block 5. Conditional operator other than equals
-
-//Each variant has: 1. At least one working function, 2. ... that contains reusable code 3. ... and is called and used correctly 4. ... and shows a grasp of encapsulation.
-
-//At least one function: 1. Accepts at least 3 parameters 2. Is called with the correct arguments
-
-//1. At least one function returns a value. 2. Return values are meaningful. 3. Return values are assigned or used in a meaningful manner.
 
 var planet= prompt('What planet are you on right now?');
 
@@ -29,3 +20,7 @@ function dinosuarGen(planet){
     }
     return 'Welcome to the old ages of ' +planet + '!';
 }
+
+console.log("You have been taken back in time to the age of the dinosaurs! Look there\'s a ...");
+
+console.log(dinosuarGen(planet));
