3d72decea0daf971a57ccb209d56aec0b7e15992
samanthaklindt <samanthaklindt@Samanthas-MacBook-Pro-3.local>
2013-11-21 14:20:36 -0500

    Commit #11
    Functions Assignment

--- Functions_Assignment/personal.js
+++ Functions_Assignment/personal.js
@@ -4,29 +4,8 @@
 
 //Personal
 
-//Some type of math is used an a calculation and includes at least 2 factors as variables, not hard-coded values.
 
-//Examples of: 1. Conditional (ternary) operator 2. If statement, condition, and True block 3. Else If statement, condition, and block 4. Else block 5. Conditional operator other than equals
-
-//Each variant has: 1. At least one working function, 2. ... that contains reusable code 3. ... and is called and used correctly 4. ... and shows a grasp of encapsulation.
-
-//At least one function: 1. Accepts at least 3 parameters 2. Is called with the correct arguments
-
-//1. At least one function returns a value. 2. Return values are meaningful. 3. Return values are assigned or used in a meaningful manner.
 
 var color;
 color= prompt('What is your favorite color?');
 
-function colorGen(color){
-
-    if(color = 'red'){
-        console.log('Your favorite color is red, like an apple!')
-    else if(color = 'blue')
-            console.log('Your favorite color is blue, like the sky!');
-    }else if(color = green){
-        console.log('Your favorite color is green, like the grass!');
-    }
-    return color;
-}
-
-console.log(colorGen());
